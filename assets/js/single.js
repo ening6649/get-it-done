2d88620ff6521a47747b2e36ec9732a8a2574b69
ening <ening6649@yahoo.com>
2021-11-02 12:58:12 -0400

    added link between pages

--- assets/js/single.js
+++ assets/js/single.js
@@ -1,5 +1,24 @@
 
 var issueContainerEl = document.querySelector("#issues-container");
+var limitWarningEl = document.querySelector("#limit-warning");
+var repoNameEl = document.querySelector("#repo-name");
+
+var getRepoName= function() {
+    // grab repo name from url query string
+    var queryString = document.location.search;
+    
+    var repoName = queryString.split("=")[1];
+    if (repoName) {
+        // display repo name on the page
+     getRepoIssues(repoName);
+     repoNameEl.textContent = repoName;
+    } else {
+        // if no repo was given, redirect to the homepage 
+     document.location.replace("./index.html");
+    }
+    console.log(repoName);
+} 
+
 
 var getRepoIssues = function(repo) {
     console.log(repo);
@@ -11,15 +30,19 @@ var getRepoIssues = function(repo) {
           response.json().then(function(data) {
             // pass response data to dom function
             displayIssues(data);
+            // check if api has paginated issues
+            if (response.headers.get("Link")) {
+                displayWarning(repo);
+            }
           });
         }
         else {
-          alert("There was a problem with your request!");
+            document.location.replace("./index.html");
         }
     });
 };
   
-  getRepoIssues("facebook/react");
+  
 
 var displayIssues = function(issues) {
     // if the repo has no issue , the if statement will fire
@@ -58,3 +81,20 @@ var displayIssues = function(issues) {
         issueContainerEl.appendChild(issueEl);
     }
 };
+
+var displayWarning = function(repo) {
+    // add text to warning container
+    limitWarningEl.textContent = "To see more than 30 issues, visit ";
+    // append a link element with a href attribute that poiunts to issues
+    // https://github.com/<repo>/issues
+    var linkEl = document.createElement("a");
+    linkEl.textContent = "See More Issues on GitHub.com";
+    linkEl.setAttribute("href", "https://github.com/" + repo + "/issues");
+    linkEl.setAttribute("target", "_blank");
+  
+    // append to warning container
+    limitWarningEl.appendChild(linkEl);
+};
+
+getRepoName();
+// HTTP headers allow the client and the server to pass additional information with an HTTP request or response.
